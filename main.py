458e6318659bf2a8c68cd431e05bf91e19991bc3
Jupiee <mzkplayz1@gmail.com>
2026-03-05 22:27:27 +0500

    feat: Add token embedding

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from preprocessor import create_dataloader
+import torch
+
+vocab_size = 50257
+output_dim = 256
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 
 with open("the-verdict.txt", "r") as file:
 
     raw_text = file.read()
 
-dataloader = create_dataloader(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+dataloader = create_dataloader(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
 
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-second_batch = next(data_iter)
-print(first_batch)
-print(second_batch)
+first_batch_input, first_batch_output = next(data_iter)
+print(f"Inputs: {first_batch_input}\nOutput: {first_batch_output}")
+
+token_embeddings = embedding_layer(first_batch_input)
+print(token_embeddings.shape)
