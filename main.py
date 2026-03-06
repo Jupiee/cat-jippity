424af1bfc1001441225c52531b06497f7243b49d
Jupiee <mzkplayz1@gmail.com>
2026-03-06 21:36:49 +0500

    feat: add positional embeddings into token embeddings

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ with open("the-verdict.txt", "r") as file:
 
     raw_text = file.read()
 
-dataloader = create_dataloader(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+max_length = 4
+
+dataloader = create_dataloader(raw_text, batch_size=8, max_length=max_length, stride=4, shuffle=False)
 
 data_iter = iter(dataloader)
 first_batch_input, first_batch_output = next(data_iter)
@@ -17,3 +19,11 @@ print(f"Inputs: {first_batch_input}\nOutput: {first_batch_output}")
 
 token_embeddings = embedding_layer(first_batch_input)
 print(token_embeddings.shape)
+
+context_length = max_length
+positional_emb_layer = torch.nn.Embedding(context_length, output_dim)
+positional_embeddings = positional_emb_layer(torch.arange(context_length))
+print(positional_embeddings.shape)
+
+input_embeddings = token_embeddings + positional_embeddings
+print(input_embeddings.shape)
