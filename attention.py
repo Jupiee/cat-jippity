81b03e22a28ed1ffe41f3f15c088d7b972f58121
Jupiee <mzkplayz1@gmail.com>
2026-03-14 02:20:24 +0500

    feat: Extend Casual Attention to MultiHead Attention

--- attention.py
+++ attention.py
@@ -1,13 +1,19 @@
 import torch
 import torch.nn as nn
 
-class SelfAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
+
+        assert (d_out % num_heads == 0), "Output dimensions must be divisible by number of heads"
+
         self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
         self.dropout = nn.Dropout(dropout)
 
         self.register_buffer(
@@ -22,15 +28,25 @@ class SelfAttention(nn.Module):
         keys = self.W_key(x)
         values = self.W_value(x)
         
-        attn_scores = queries @ keys.transpose(1, 2)
-        attn_scores.masked_fill_(
-            self.mask.bool()[:num_tokens, :num_tokens],
-            -torch.inf
-        )
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
         attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
 
-        context_vec = attn_weights @ values
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
 
         return context_vec
 
@@ -43,12 +59,13 @@ inputs = torch.tensor(
     [0.05, 0.80, 0.55]] # step (x^6)
 )
 
-d_in = inputs.shape[1]
-d_out = 2
-
 torch.manual_seed(123)
 batch = torch.stack((inputs, inputs), dim=0)
-context_length = batch.shape[1]
-casual_attn = SelfAttention(d_in, d_out, context_length, 0.0)
-context_vectors = casual_attn(batch)
+
+batch_size , context_length, d_in = batch.shape
+d_out = 2
+
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vectors = mha(batch)
+print(context_vectors)
 print(f"context vector shape: {context_vectors.shape}")
