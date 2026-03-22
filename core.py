00fd007c085293101ae10b08cc2605d1f003eb62
Jupiee <mzkplayz1@gmail.com>
2026-03-22 20:43:59 +0500

    feat: Implement GELU and FeedForward layers

--- core.py
+++ core.py
@@ -58,6 +58,30 @@ class LayerNorm(nn.Module):
 
         return self.scale * norm_x + self.shift
     
+class GELU(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0/torch.pi)) * (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+class FeedForward(nn.Module):
+
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["embedding_dim"], 4 * cfg["embedding_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["embedding_dim"], cfg["embedding_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+        
 tokenizer = tiktoken.get_encoding("gpt2")
 batch =[]
 txt1 = "Every effort moves you"
@@ -92,3 +116,8 @@ mean = out_ln.mean(dim=-1, keepdim=True)
 var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 print(f"Mean:\n{mean}")
 print(f"Variance:\n{var}")
+
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)
+out = ffn(x)
+print(out.shape)
