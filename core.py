d6ff5fc99ec378937813eaa56b0ed09b942c2576
Jupiee <mzkplayz1@gmail.com>
2026-03-23 22:40:17 +0500

    feat: Implement example neural network with shortcut connections

--- core.py
+++ core.py
@@ -81,7 +81,58 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
-        
+    
+class ExampleDeepNeuralNetwork(nn.Module):
+
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), GELU()),
+        ])
+
+    def forward(self, x):
+
+        for layer in self.layers:
+            layer_output = layer(x)
+
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            
+            else:
+                x = layer_output
+
+        return x
+    
+def print_gradients(model, x):
+
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+layer_sizes = [3, 3, 3, 3, 3, 1]
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)
+model_without_shortcut = ExampleDeepNeuralNetwork(layer_sizes, False)
+print_gradients(model_without_shortcut, sample_input)
+
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(layer_sizes, True)
+print_gradients(model_with_shortcut, sample_input)
+
 tokenizer = tiktoken.get_encoding("gpt2")
 batch =[]
 txt1 = "Every effort moves you"
