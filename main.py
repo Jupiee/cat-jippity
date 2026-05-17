939d2be1d9cb2cb6e2d1767f4d8cf07766060577
Jupiee <mzkplayz1@gmail.com>
2026-05-17 15:46:21 +0500

    feat: Implement basic text generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from preprocessor import create_dataloader
-import torch
+from core import GPTModel, generate_text_simple
+import torch, tiktoken
 
 vocab_size = 50257
 output_dim = 256
@@ -27,3 +28,47 @@ print(positional_embeddings.shape)
 
 input_embeddings = token_embeddings + positional_embeddings
 print(input_embeddings.shape)
+
+if __name__ == '__main__':
+
+    def text_to_tokens(text, tokenizer):
+
+        encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+        encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+
+        return encoded_tensor
+
+    def token_to_text(token_ids, tokenizer):
+
+        flat = token_ids.squeeze(0)
+        
+        return tokenizer.decode(flat.tolist())
+
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257, # Vocabulary size
+        "context_length": 256, # Context length
+        "embedding_dim": 768, # Embedding dimension
+        "n_heads": 12, # Number of attention heads
+        "n_layers": 12, # Number of layers
+        "drop_rate": 0.1, # Dropout rate
+        "qkv_bias": False # Query-Key-Value bias
+    }
+
+    torch.manual_seed(123)
+
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+
+    start_context = "Every effort moves you"
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    tokens = text_to_tokens(start_context, tokenizer)
+
+    token_ids = generate_text_simple(
+        model,
+        tokens,
+        10,
+        GPT_CONFIG_124M["context_length"]
+    )
+
+    print(f"Output text:\n{token_to_text(token_ids, tokenizer)}")
